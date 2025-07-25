20e2e7c27e3fb0bbde2c0f28103041780fb9c50f
Jan Janoušek <janousek@ocel.cz>
2025-07-25 10:47:06 +0200

    Files scoped namespace, fixed indentation

--- src/Hangfire.Storage.MySql/JobQueue/MySqlJobQueueMonitoringApi.cs
+++ src/Hangfire.Storage.MySql/JobQueue/MySqlJobQueueMonitoringApi.cs
@@ -3,47 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using Dapper;
 
-namespace Hangfire.Storage.MySql.JobQueue
+namespace Hangfire.Storage.MySql.JobQueue;
+
+internal class MySqlJobQueueMonitoringApi : IPersistentJobQueueMonitoringApi
 {
-    internal class MySqlJobQueueMonitoringApi : IPersistentJobQueueMonitoringApi
+
+  private static readonly TimeSpan QueuesCacheTimeout = TimeSpan.FromSeconds(5);
+  private readonly object _cacheLock = new object();
+  private List<string> _queuesCache = new List<string>();
+  private DateTime _cacheUpdated;
+
+  private readonly MySqlStorage _storage;
+  private readonly MySqlStorageOptions _storageOptions;
+
+  public MySqlJobQueueMonitoringApi(MySqlStorage storage, MySqlStorageOptions storageOptions)
+  {
+    _storage = storage ?? throw new ArgumentNullException(nameof(storage));
+    _storageOptions = storageOptions;
+  }
+
+  public IEnumerable<string> GetQueues()
+  {
+    lock (_cacheLock)
     {
-        private static readonly TimeSpan QueuesCacheTimeout = TimeSpan.FromSeconds(5);
-        private readonly object _cacheLock = new object();
-        private List<string> _queuesCache = new List<string>();
-        private DateTime _cacheUpdated;
+      if (_queuesCache.Count == 0 || _cacheUpdated.Add(QueuesCacheTimeout) < DateTime.UtcNow)
+      {
+        var result = _storage.UseConnection(connection =>
+            connection
+                .Query($"select distinct(Queue) from `{_storageOptions.TablesPrefix}JobQueue`")
+                .Select(x => (string)x.Queue)
+                .ToList());
 
-        private readonly MySqlStorage _storage;
-        private readonly MySqlStorageOptions _storageOptions;
+        _queuesCache = result;
+        _cacheUpdated = DateTime.UtcNow;
+      }
 
-        public MySqlJobQueueMonitoringApi(MySqlStorage storage, MySqlStorageOptions storageOptions)
-        {
-            _storage = storage ?? throw new ArgumentNullException(nameof(storage));
-            _storageOptions = storageOptions;
-        }
+      return _queuesCache.ToList();
+    }
+  }
 
-        public IEnumerable<string> GetQueues()
-        {
-            lock (_cacheLock)
-            {
-                if (_queuesCache.Count == 0 || _cacheUpdated.Add(QueuesCacheTimeout) < DateTime.UtcNow)
-                {
-                    var result = _storage.UseConnection(connection => 
-                        connection
-                            .Query($"select distinct(Queue) from `{_storageOptions.TablesPrefix}JobQueue`")
-                            .Select(x => (string)x.Queue)
-                            .ToList());
-
-                    _queuesCache = result;
-                    _cacheUpdated = DateTime.UtcNow;
-                }
-
-                return _queuesCache.ToList();
-            }
-        }
-
-        public IEnumerable<int> GetEnqueuedJobIds(string queue, int @from, int perPage)
-        {
-            string sqlQuery = $@"
+  public IEnumerable<int> GetEnqueuedJobIds(string queue, int @from, int perPage)
+  {
+    string sqlQuery = $@"
 SET @rank=0;
 select r.JobId from (
   select jq.JobId, @rank := @rank+1 AS `rank` 
@@ -53,30 +54,29 @@ select r.JobId from (
 ) as r
 where r.`rank` between @start and @end;";
 
-            return _storage.UseConnection(connection =>
+    return _storage.UseConnection(connection =>
+        connection.Query<int>(
+            sqlQuery,
+            new { queue = queue, start = @from + 1, end = @from + perPage }));
+  }
+
+  public IEnumerable<int> GetFetchedJobIds(string queue, int @from, int perPage)
+  {
+    return Enumerable.Empty<int>();
+  }
+
+  public EnqueuedAndFetchedCountDto GetEnqueuedAndFetchedCount(string queue)
+  {
+    return _storage.UseConnection(connection =>
+    {
+      var result =
                 connection.Query<int>(
-                    sqlQuery,
-                    new {queue = queue, start = @from + 1, end = @from + perPage}));
-        }
+                    $"select count(Id) from `{_storageOptions.TablesPrefix}JobQueue` where Queue = @queue", new { queue = queue }).Single();
 
-        public IEnumerable<int> GetFetchedJobIds(string queue, int @from, int perPage)
-        {
-            return Enumerable.Empty<int>();
-        }
-
-        public EnqueuedAndFetchedCountDto GetEnqueuedAndFetchedCount(string queue)
-        {
-            return _storage.UseConnection(connection =>
-            {
-                var result = 
-                    connection.Query<int>(
-                        $"select count(Id) from `{_storageOptions.TablesPrefix}JobQueue` where Queue = @queue", new { queue = queue }).Single();
-
-                return new EnqueuedAndFetchedCountDto
-                {
-                    EnqueuedCount = result,
-                };
-            });
-        }
-    }
+      return new EnqueuedAndFetchedCountDto
+      {
+        EnqueuedCount = result,
+      };
+    });
+  }
 }
