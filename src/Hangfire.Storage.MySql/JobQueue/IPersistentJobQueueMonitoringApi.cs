7f99759c7049e213f86be5360f855bfc97e0fe89
Jan Janoušek <janousek@ocel.cz>
2025-08-12 09:25:56 +0200

    Add comment to IPersistentJobQueueMonitoringApi
    Added a comment to the IPersistentJobQueueMonitoringApi interface in the Hangfire.Storage.MySql.JobQueue namespace. The comment suggests that returning lists may be a better approach for the GetQueues method, providing guidance for future development or refactoring.

--- src/Hangfire.Storage.MySql/JobQueue/IPersistentJobQueueMonitoringApi.cs
+++ src/Hangfire.Storage.MySql/JobQueue/IPersistentJobQueueMonitoringApi.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace Hangfire.Storage.MySql.JobQueue;
+﻿namespace Hangfire.Storage.MySql.JobQueue;
 
 public interface IPersistentJobQueueMonitoringApi
 {
+  // TODO: return Lists is probably the better idea
   IEnumerable<string> GetQueues();
   IEnumerable<int> GetEnqueuedJobIds(string queue, int from, int perPage);
   IEnumerable<int> GetFetchedJobIds(string queue, int from, int perPage);
