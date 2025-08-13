7424a2b210d075b0780ce4e5788babae11872895
Jan Janoušek <janousek@ocel.cz>
2025-08-13 16:12:52 +0200

    Added TestApp

---
+++ src/TestApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using Hangfire.Storage.MySql;
+
+const string connectionString = "Server=devubu;Database=hf1;Uid=hf1;Pwd=sporak";
+
+var s = new MySqlStorage(connectionString, new MySqlStorageOptions(), MySqlConnector.MySqlConnectorFactory.Instance);
+
+var monitor = s.GetMonitoringApi();
+var stats = monitor.GetStatistics();
+
+Console.WriteLine("Ende");
