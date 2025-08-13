372015e81ba056327dfd07a41b10cfd0f67d4332
Jan Janoušek <janousek@ocel.cz>
2025-08-13 15:32:21 +0200

    Add AluCollectionExtensions with GetValueOrDefault method
    Introduced a new static class `AluCollectionExtensions` in the `System.Collections.Generic` namespace. Added the `GetValueOrDefault` method to extend `IReadOnlyDictionary<TKey, TValue>`, allowing retrieval of values with a default fallback. Included a null check for the dictionary and conditionally compiled the method for compatibility with .NET versions prior to 5.0.

---
+++ src/Hangfire.Storage.MySql/AluCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace System.Collections.Generic;
+
+static class AluCollectionExtensions
+{
+#if !NET5_0_OR_GREATER
+  public static TValue GetValueOrDefault<TKey, TValue>(
+    this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+  {
+    if (dictionary is null)
+    {
+      throw new ArgumentNullException(nameof(dictionary));
+    }
+    return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+  }
+#endif
+}
